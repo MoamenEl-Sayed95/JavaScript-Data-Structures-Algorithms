f734ce0570e55cc822f1ec3631900a0e00d411de
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-07-09 16:37:11 +0300

    Update All

--- Linked-Lists/LL-Interview-Leetcode-Exercises/Find-Middle-Node/EXERCISE-LL-Find-Middle-Node.js
+++ Linked-Lists/LL-Interview-Leetcode-Exercises/Find-Middle-Node/EXERCISE-LL-Find-Middle-Node.js
@@ -54,17 +54,12 @@ class LinkedList {
     }
     
     // WRITE THE FINDMIDDLENODE METHOD HERE // 
-    findMiddleNode() {
-        let slow = this.head;
-        let fast = this.head;
-
-        while (fast !== null && fast.next !== null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        return slow;
-    }
+    //                                      //
+    //                                      //
+    //                                      //
+    //                                      // 
+    //                                      //
+    //////////////////////////////////////////
 }
 
 
@@ -115,44 +110,3 @@ console.log(`\nMiddle node value of list 2: ${middleNode2.value}`);
     6
     Middle node value of list 2: 4
 */
-
-
-// --------------------------------------------------
-
-
-//===========================================================
-//================ Middle of the Linked List ================
-//===========================================================
-
-//===========================================================
-// https://leetcode.com/problems/middle-of-the-linked-list/
-//===========================================================
-
-// Given the head of a singly linked list, return the middle node of the linked list.
-
-// If there are two middle nodes, return the second middle node.
-
-
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
-
-// var middleNode = function(head) {
-//     let slow = head;
-//     let fast = head;
-    
-//     while (fast !== null && fast.next !== null) {
-//         slow = slow.next;
-//         fast = fast.next.next;
-//     }
-//     return slow;
-// };
--- Linked-Lists/LL-Interview-Leetcode-Exercises/Has-Loop/EXERCISE-LL-Has-Loop.js
+++ Linked-Lists/LL-Interview-Leetcode-Exercises/Has-Loop/EXERCISE-LL-Has-Loop.js
@@ -59,25 +59,16 @@ class LinkedList {
         this.length++;
     }
     
-    // WRITE HAS LOOP METHOD HERE // 
-    hasLoop() {
-        let slow = this.head;
-        let fast = this.head;
-
-        while (fast !== null && fast.next !== null) {
-            slow = slow.next;
-            fast = fast.next.next;
-
-            if (slow === fast) {
-                return true;
-            }
-        }
-
-        return false;
-    }
+    // WRITE THE FINDMIDDLENODE METHOD HERE // 
+    //                                      //
+    //                                      //
+    //                                      //
+    //                                      // 
+    //                                      //
+    ////////////////////////////////////////// 
+     
 
 }
-
 
 
 let myLinkedList = new LinkedList(1);
@@ -111,47 +102,3 @@ console.log(`\nHas loop after creating a loop? ${hasLoopResultAfterLoop}`);
     Has loop? false
     Has loop after creating a loop? true
 */
-
-
-// ----------------------------------------------
-
-
-//==========================================
-//================ Has Loop ================
-//==========================================
-
-//=================================================
-// https://leetcode.com/problems/linked-list-cycle/
-//=================================================
-
-// Given head, the head of a linked list, determine if the linked list has a cycle in it.
-
-// There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
-
-// Return true if there is a cycle in the linked list. Otherwise, return false.
-
-/**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
- */
-
-/**
- * @param {ListNode} head
- * @return {boolean}
- */
-
-// var hasCycle = function (head) {
-//   let slow = head;
-//   let fast = head;
-
-//   while (fast !== null && fast.next !== null) {
-//     slow = slow.next;
-//     fast = fast.next.next;
-
-//     if (slow === fast) return true;
-//   }
-//   return false;
-// };
