a78875e95507459cd16e0c740a184d2bea50512e
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-07-09 14:45:05 +0300

    Add LL-Solutions

--- Linked-Lists/LL-Interview-Leetcode-Exercises/Find-Middle-Node/SOLUTION-LL-Find-Middle-Node.js
+++ Linked-Lists/LL-Interview-Leetcode-Exercises/Find-Middle-Node/SOLUTION-LL-Find-Middle-Node.js
@@ -6,6 +6,11 @@
 // https://leetcode.com/problems/middle-of-the-linked-list/
 //===========================================================
 
+// Given the head of a singly linked list, return the middle node of the linked list.
+
+// If there are two middle nodes, return the second middle node.
+
+
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
