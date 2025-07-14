3fb8ba34ddad659d7f68228889bd5039a636cc3c
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-07-14 13:49:21 +0300

    Add S&Q-ILE

---
+++ Stacks-Queues/S&Q-ILE/Stack-Push-for-a-Stack-That-Uses-an-Array/SOLUTION-Push-for-a-Stack-That-Uses-an-Array.js
@@ -0,0 +1,37 @@
+class Stack {
+  constructor() {
+    this.stackList = [];
+  }
+
+  getStackList() {
+    return this.stackList;
+  }
+
+  printStack() {
+    for (let i = this.stackList.length - 1; i >= 0; i--) {
+      console.log(this.stackList[i]);
+    }
+  }
+
+  isEmpty() {
+    return this.stackList.length === 0;
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return null;
+    } else {
+      return this.stackList[this.stackList.length - 1];
+    }
+  }
+
+  size() {
+    return this.stackList.length;
+  }
+
+  push(value) {
+    // Your implementation goes here
+    this.stackList.push(value);
+    return this;
+  }
+}
