0de5f075c2d96c82d5e9522ec0c8374bc0af28fd
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-07-15 15:12:51 +0300

    Add New Section Hash-Tables<HT-Constructor>

--- Hash-Tables/HT-Constructor/SOLUTION-HT-Constructor.js
+++ Hash-Tables/HT-Constructor/SOLUTION-HT-Constructor.js
@@ -1,36 +1,31 @@
 class HashTable {
-    constructor(size = 7) {
-        this.dataMap = new Array(size);
-    }
-   
-    _hash(key) {
-        let hash = 0;
-        for (let i = 0; i < key.length; i++) {
-            hash = (hash + key.charCodeAt(i) * 23) % this.dataMap.length;
-        }
-        return hash;
-    }
+  constructor(size = 7) {
+    this.dataMap = new Array(size);
+  }
 
-    printTable() {
-        for (let i = 0; i < this.dataMap.length; i++) {
-            console.log(i, ": ", this.dataMap[i]);
-        }
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * 23) % this.dataMap.length;
     }
+    return hash;
+  }
 
+  printTable() {
+    for (let i = 0; i < this.dataMap.length; i++) {
+      console.log(i, ": ", this.dataMap[i]);
+    }
+  }
 }
-
-
 
 function test() {
-    let myHashTable = new HashTable();
-    myHashTable.printTable();  
+  let myHashTable = new HashTable();
+  myHashTable.printTable();
 }
-
 
 test();
 
-    
-    /*
+/*
         EXPECTED OUTPUT:
         ----------------
         0 :  undefined
