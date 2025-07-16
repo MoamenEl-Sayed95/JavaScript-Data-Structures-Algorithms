bcbc7924317cf18f5771bc0995eabf0de3974acc
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-07-16 12:16:58 +0300

    Add New Section Hash-Tables<HT-ILE>

---
+++ Hash-Tables/HT-ILE/HT-First-Non-Repeating-Character/SOLUTION-HT-First-Non-Repeating-Character.js
@@ -0,0 +1,63 @@
+function firstNonRepeatingChar(str) {
+  const charCounts = new Map();
+
+  for (let c of str) {
+    charCounts.set(c, (charCounts.get(c) || 0) + 1);
+  }
+
+  for (let c of str) {
+    if (charCounts.get(c) === 1) {
+      return c;
+    }
+  }
+
+  return null;
+}
+
+// ---------------
+// All Unique
+// ---------------
+console.log("All Unique:");
+console.log("Input: 'abcde'");
+console.log("Output: ", JSON.stringify(firstNonRepeatingChar("abcde")));
+console.log("---------------");
+
+// ---------------
+// Some Duplicates
+// ---------------
+console.log("Some Duplicates:");
+console.log("Input: 'aabbccdef'");
+console.log("Output: ", JSON.stringify(firstNonRepeatingChar("aabbccdef")));
+console.log("---------------");
+
+// ---------------
+// All Duplicates
+// ---------------
+console.log("All Duplicates:");
+console.log("Input: 'aabbcc'");
+console.log("Output: ", JSON.stringify(firstNonRepeatingChar("aabbcc")));
+console.log("---------------");
+
+// ---------------
+// Empty String
+// ---------------
+console.log("Empty String:");
+console.log("Input: ''");
+console.log("Output: ", JSON.stringify(firstNonRepeatingChar("")));
+console.log("---------------");
+
+// ---------------
+// Single Character
+// ---------------
+console.log("Single Character:");
+console.log("Input: 'a'");
+console.log("Output: ", JSON.stringify(firstNonRepeatingChar("a")));
+console.log("---------------");
+
+// ---------------
+// Multiple Non-Repeating Chars
+// ---------------
+console.log("Multiple Non-Repeating Chars:");
+console.log("Input: 'abcdaef'");
+console.log("Output: ", JSON.stringify(firstNonRepeatingChar("abcdaef")));
+console.log("---------------");
