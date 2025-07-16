80f9814070b13fb2cadb0d3bf78d2e94f83f7f58
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-07-16 11:36:12 +0300

    Add New Section Hash-Tables<HT-ILE>

---
+++ Hash-Tables/HT-ILE/SOLUTION-HT-Item-In-Common.js
@@ -0,0 +1,72 @@
+function itemInCommon(arr1, arr2) {
+    const map = new Map();
+
+    for (let i of arr1) {
+        map.set(i, true);
+    }
+
+    for (let j of arr2) {
+        if (map.has(j)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function itemInCommon(arr1, arr2) {
+    const obj = {};
+
+    for (let i of arr1) {
+        obj[i] = true;
+    }
+
+    for (let j of arr2) {
+        if (obj[j]) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+// ---------------
+// One Common Item
+// ---------------
+console.log("One Common Item:");
+console.log("Input: [1, 3, 5], [2, 4, 5]");
+console.log("Output: ", itemInCommon([1, 3, 5], [2, 4, 5]));
+console.log("---------------");
+
+// ---------------
+// No Common Items
+// ---------------
+console.log("No Common Items:");
+console.log("Input: [1, 3, 5], [2, 4, 6]");
+console.log("Output: ", itemInCommon([1, 3, 5], [2, 4, 6]));
+console.log("---------------");
+
+// ---------------
+// Multiple Common Items
+// ---------------
+console.log("Multiple Common Items:");
+console.log("Input: [1, 2, 3], [2, 3, 4]");
+console.log("Output: ", itemInCommon([1, 2, 3], [2, 3, 4]));
+console.log("---------------");
+
+// ---------------
+// Empty Arrays
+// ---------------
+console.log("Empty Arrays:");
+console.log("Input: [], []");
+console.log("Output: ", itemInCommon([], []));
+console.log("---------------");
+
+// ---------------
+// One Empty Array
+// ---------------
+console.log("One Empty Array:");
+console.log("Input: [1, 2, 3], []");
+console.log("Output: ", itemInCommon([1, 2, 3], []));
+console.log("---------------");
